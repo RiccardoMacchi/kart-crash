c189a405e2dc82ad3d54228a805955410745fbc1
RiccardoMacchi <rick.mac97@outlook.com>
2024-07-29 20:28:00 +0200

    kart-crush

---
+++ script.js
@@ -0,0 +1,281 @@
+// Recupero elementi in pagina
+// Recupero griglia ostacoli
+const grid = document.querySelector('.grid');
+// Recupero bottoni
+const btnLeft = document.querySelector('.left-button');
+const btnRigth = document.querySelector('.right-button');
+// Recupero counter punti
+const scoreCounter = document.querySelector('.score-counter');
+// Recupero schermata fine gioco
+const endGame = document.querySelector('.end-game-screen');
+// Recupero punteggio finale
+const finalScore = document.querySelector('.final-score');
+// Recupero pulsante gioca ancora
+const playAgain = document.querySelector('.play-again')
+
+
+
+// Griglia di gioco
+const gridMatrix = [
+    ['', '', '', '', '', 'grass', ''],
+    ['', 'cones', '', '', '', '', 'fence'],
+    ['', '', 'rock', '', '', '', ''],
+    ['fence', '', '', '', '', '', ''],
+    ['', '', 'grass', '', '', 'water', ''],
+    ['', '', '', '', 'cones', '', ''],
+    ['', 'water', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', 'rock', ''],
+];
+
+console.table(gridMatrix);
+
+// Impostazioni inizali del kart
+let ptnToBonus = 0;
+let activeBonus = false;
+let score = 0;
+let speed = 1000;
+const kartPosition = {
+    y: 7,
+    x: 3,
+}
+
+function renderGrid() {
+    // Pulizia della griglia per il movimento
+    grid.innerHTML = '';
+    // Recupero di ogni cella nella riga
+    gridMatrix.forEach(function (singleCells) {
+        singleCells.forEach(function (contentCells) {
+            // Creazione div 
+            const cell = document.createElement('div');
+            // Classe div
+            cell.className = 'cell'
+            // Controllo per aggiunta elementi visivi
+            if (contentCells) cell.classList.add(contentCells);
+
+            grid.appendChild(cell)
+        })
+    })
+}
+
+// Funzioni di render
+function renderElements() {
+    placeKart();
+
+    renderGrid();
+}
+
+
+// Funzioni kart
+
+// Posizione kart
+function placeKart() {
+    // Recupero del valore della cella in cui andrà il kart
+    const contenteBeforeKart = gridMatrix[kartPosition.y][kartPosition.x]
+
+    // Controllo se c'è qualcosa collisione se c'è moneta punti bonus
+    if (contenteBeforeKart === 'coin') {
+        getBonus()
+    } else if (contenteBeforeKart) {
+        gameOver()
+    }
+    // Posiziono il kart se non è gameover
+    gridMatrix[kartPosition.y][kartPosition.x] = 'kart';
+}
+
+// Muovi il kart
+function moveKart(direction) {
+
+    // rimozione precedente del kart
+
+    gridMatrix[kartPosition.y][kartPosition.x] = '';
+
+    // Condizioni di movimento
+    switch (direction) {
+        case 'left':
+            if (kartPosition.x > 0) kartPosition.x--;
+            break;
+        case 'right':
+            if (kartPosition.x < 6) kartPosition.x++;
+            break;
+        default:
+            gridMatrix[kartPosition.y][kartPosition.x] = 'kart';
+    }
+
+    // Render elemenenti kart e grid
+
+    renderElements();
+}
+
+// Funzione per movimento ostacoli
+function scrollOsbtacles() {
+    // Rimozione temporane kart
+    gridMatrix[kartPosition.y][kartPosition.x] = '';
+
+    // Se ci sono monete
+    let coinInGame = coinInGrid()
+
+    // Recupero ultima riga
+    let lastRow = gridMatrix.pop();
+
+
+    // Aggiunta della moneta alla ristampa con controllo se è la sola nel grid
+    if (activeBonus) lastRow = moreBonus(lastRow);
+    else if (!coinInGame) lastRow = insertCoin(lastRow);
+
+    // Mescolazione random della lista ostacoli
+    shuffleElements(lastRow);
+
+    // Riportarla in cima
+    gridMatrix.unshift(lastRow)
+
+    // Renderizziamo tutto kart e griglia
+    renderElements()
+}
+
+// Funzione mescolata random
+function shuffleElements(row) {
+    for (let i = row.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [row[i], row[j]] = [row[j], row[i]];
+    }
+
+    return row;
+}
+
+// Funzioni per punti e volcita
+// Incremento punti
+function incrementScore() {
+    scoreCounter.innerHTML = ++score;
+}
+
+// Incremento velocità
+function incrementSpeed() {
+    if (speed > 100) {
+        // Blocco del flusso
+        clearInterval(gameLoop)
+        // Aumento velocita dimnuendo il tempo dell'interval
+        speed -= 100;
+        // riparte il flusso 
+        gameLoop = setInterval(runGameFlow, speed)
+    }
+}
+
+
+
+// Funzione di operazioni da ripetere ciclicamente
+function runGameFlow() {
+    // Aumento velocità solo dopo ogni 10 punti
+    if (score % 20 === 0) incrementSpeed();
+
+    // Aumento punti
+    incrementScore()
+
+    // Scrollo degli ostacoli
+    scrollOsbtacles()
+}
+
+
+// Evento di gioco
+btnLeft.addEventListener('click', function () {
+    moveKart('left')
+})
+
+btnRigth.addEventListener('click', function () {
+    moveKart('right')
+})
+
+// Reazione alle frecce della tastiera
+document.addEventListener('keyup', function (e) {
+    switch (e.key) {
+
+        case 'ArrowLeft':
+            moveKart('left')
+            break;
+        case 'ArrowRight':
+            moveKart('right')
+            break;
+        default: return;
+
+    }
+})
+
+// Gioca ancora
+playAgain.addEventListener('click', function () {
+    location.reload();
+})
+
+// Funzione fine partita
+
+function gameOver() {
+    clearInterval(gameLoop);
+
+    // Stampa in pagina del punteggio finale
+    finalScore.innerHTML = score;
+    // Rimozione classe hidden
+    endGame.classList.remove('hidden');
+    // focus sul bottone di gioca ancora cliccando enter si rigioca
+    playAgain.focus();
+
+}
+
+
+// Funzioni bonus
+function getBonus() {
+    // Aggiunta level bonus
+    ptnToBonus++;
+    if (ptnToBonus > 5 && ptnToBonus < 10) {
+        activeBonus = true
+    } else {
+        activeBonus = false
+    }
+    score += 25;
+    scoreCounter.innerHTML = score;
+    // Aggiunta classe per efftto grafico
+    scoreCounter.classList.add('bonus');
+    // Rimozione classe per evenutali future monete
+    setTimeout(function () {
+        scoreCounter.classList.remove('bonus')
+    }, 1000)
+}
+
+// Funzione bonus bonus
+
+function moreBonus(row) {
+    console.log("row")
+    for (let i = 0; i < row.length - 1; i++) {
+
+        if (row[i] === '') {
+            row[i] = 'coin'
+
+        }
+    }
+
+    return row;
+}
+
+
+// Funzione per insiere monete
+
+function insertCoin(row) {
+    // Troviamo l'indice del primo elemento vuoto nell'array
+    const emptyElIndex = row.indexOf('');
+    // Aggiunta coin
+    row[emptyElIndex] = 'coin'
+    // Ristampa della riga
+    return row;
+}
+
+// Funzione per scoprire se c'è una moneta nella griglia
+function coinInGrid() {
+    let coinFound = false;
+    // Recupero delle righe
+    gridMatrix.forEach(function (singleRow) {
+        if (singleRow.includes('coin')) coinFound = true;
+    })
+    return coinFound;
+}
+
+
+// Scroll automatico ostacoli, punti e velocità kart
+let gameLoop = setInterval(runGameFlow, speed)
